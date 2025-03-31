90eb6777a53f848aa177e251d54978947a5da904
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2025-03-31 13:12:05 +0530

    i change some code

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask  # type: ignore
+from flask import Flask, render_template, url_for  
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
